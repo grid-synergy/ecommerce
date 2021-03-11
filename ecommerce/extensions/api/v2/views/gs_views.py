48cb6028bce66da48751272f42696442c3c7103a
Abdul Rafay Khan <arafay@knysys.com>
2021-03-11 07:41:10 +0000

    Create new API for shopping cart content for mobile

--- ecommerce/extensions/api/v2/views/gs_views.py
+++ ecommerce/extensions/api/v2/views/gs_views.py
@@ -140,9 +140,72 @@ def get_basket_content(request):
                 discount_benefit =  DynamicPercentageDiscountBenefit()
                 percentage = jwt['discount_percent']
                 discount_benefit.apply(basket,'dynamic_discount_condition',offers[0],discount_percent=percentage)
-
         checkout_response.update({'basket_total': basket.total_incl_tax, 'shipping_fee': 0.0, 'status': True, "status_code": 200})   
 
+        return Response(checkout_response)
+    except Exception as e:
+        logging.info(e)
+        return Response(str(e))
+
+
+
+@api_view(('GET',))
+def get_basket_content_mobile(request):
+
+    try:
+        checkout_response = None
+        basket = None
+        if request.GET.get('id'):
+            id = request.GET.get('id')
+        else:
+            if request.user.baskets.filter(status=Basket.OPEN).exists():
+                id = request.user.baskets.filter(status=Basket.OPEN).first().id
+            else:
+                id = -1
+
+        response = requests.get(url=settings.ECOMMERCE_URL_ROOT + '/api/v2/get-basket-detail/'+str(id) + '/')
+        checkout_response = json.loads(response.text)
+        if not request.user.baskets.filter(id=id).exists():
+            return Response({"status": False, "message": "Basket not found", "status_code": 404})
+        basket = request.user.baskets.get(id=id)
+        basket.strategy = request.strategy
+        for product in checkout_response['products']:
+            for item in product:
+                if item['code'] == 'course_key': 
+                    course_id = item['value'] 
+            lms_url = get_lms_url('/api/commerce/v2/checkout/' + course_id)
+            commerce_response  = requests.get(url=str(lms_url),headers={'Authorization' : 'JWT ' + str(request.site.siteconfiguration.access_token)})
+            commerce_response = json.loads(commerce_response.text)
+            if commerce_response['status_code'] == 200:
+                price = commerce_response['result']['modes'][0]['price']
+                sku = commerce_response['result']['modes'][0]['sku']
+                discount_applicable = commerce_response['result']['discount_applicable']
+                discounted_price = commerce_response['result']['discounted_price']
+                media = commerce_response['result']['media']['image'] 
+                category = commerce_response['result']['new_category']
+                title = commerce_response['result']['name']
+                organization = commerce_response['result']['organization']
+                course_info = {'course_id' : course_id ,'media': media, 'category': category, 'title': title, 'price': price, 'discount_applicable': discount_applicable, \
+                              'discounted_price': discounted_price, 'organization': organization, 'sku': sku, 'code': "course_details"}
+                product.append(course_info)
+
+        if waffle.flag_is_active(request, DYNAMIC_DISCOUNT_FLAG) and basket.lines.count() > 0:
+            discount_lms_url = get_lms_url('/api/discounts/')
+            lms_discount_client = EdxRestApiClient(discount_lms_url,jwt=request.site.siteconfiguration.access_token)
+            ck = basket.lines.first().product.course_id
+            user_id = basket.owner.lms_user_id
+            response = lms_discount_client.course(ck).get()
+            jwt = jwt_decode_handler(response.get('jwt'))
+            if jwt['discount_applicable']:
+                offers = Applicator().get_offers(basket, request.user, request)
+                basket.strategy = request.strategy
+                discount_benefit =  DynamicPercentageDiscountBenefit()
+                percentage = jwt['discount_percent']
+                discount_benefit.apply(basket,'dynamic_discount_condition',offers[0],discount_percent=percentage)
+        
+        checkout_response['products'] = [i for j in checkout_response['products'] for i in j if not i['code'] in ['certificate_type', 'course_key', 'id_verification_required']]
+        checkout_response.update({'basket_total': basket.total_incl_tax, 'shipping_fee': 0.0, 'status': True, "status_code": 200})   
+       
         return Response(checkout_response)
     except Exception as e:
         logging.info(e)
