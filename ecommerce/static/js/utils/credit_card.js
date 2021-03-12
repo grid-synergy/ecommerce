39daf80856f5ca4048f3b25073bf9e3f6d8d517c
Abdul Rafay Khan <arafay@knysys.com>
2021-03-12 12:07:16 +0000

    Resolved Card Validation issues: 1. Set max card length to 19 and allow spaces in card numbers. 2. Remove spaces from card numbers in javascript.

--- ecommerce/static/js/utils/credit_card.js
+++ ecommerce/static/js/utils/credit_card.js
@@ -7,6 +7,8 @@ define([], function() {
              * https://jsperf.com/credit-card-validator/7
              */
         isValidCardNumber: function(cardNumber) {
+            // Replace spaces from card number
+            cardNumber = cardNumber.replace(/\s/g, '');
             var len = cardNumber.length,
                 mul = 0,
                 prodArr = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 2, 4, 6, 8, 1, 3, 5, 7, 9]],
@@ -34,6 +36,8 @@ define([], function() {
              * @returns (object) - The credit card type name and CVN length.
              */
         getCreditCardType: function(cardNumber) {
+            //Replace spaces from card number
+            cardNumber = cardNumber.replace(/\s/g, '');
             var key,
                 matchers = {
                     amex: {
