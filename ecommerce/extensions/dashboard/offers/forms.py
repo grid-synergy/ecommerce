80aee38a65ba022c679b261ea14b18e673cb8085
Abdul Rafay <arafay.knysys@gmail.com>
2021-05-10 16:37:08 +0500

    LMA-535: Prepared all the necessary fields for the API call

--- ecommerce/extensions/dashboard/offers/forms.py
+++ ecommerce/extensions/dashboard/offers/forms.py
@@ -29,13 +29,27 @@ class BenefitForm(CoreBenefitForm):
         if ConditionalOffer.objects.filter(benefit=response.id).exists():
             offer = ConditionalOffer.objects.filter(benefit=response.id)[0]
 
-        if offer and len(offer.condition.range.all_products()) == 1 and offer.benefit.type == 'Percentage':
-            site =  Site.objects.get_current()
-            product = offer.condition.range.all_products()[0]
-            sku = product.stockrecords.first().partner_sku
-            data = {'discount_percentage': float(response.value)}
-            commerce_api_client = site.siteconfiguration.lhub_commerce_api_client
-            update_discount_response  = commerce_api_client.update_discount(sku).post(data=data)
+        if offer:
+            ecommerce_data = {}
+            ecommerce_data["start_datetime"] = offer.start_datetime
+            ecommerce_data["end_datetime"] = offer.end_datetime
+            ecommerce_data["priority"] = offer.priority
+            ecommerce_data["incentive_type"] = offer.benefit.type
+            ecommerce_data["incentive_value"] = offer.benefit.value
+            ecommerce_data["condition_type"] = offer.condition.type
+            ecommerce_data["condition_value"] = offer.condition.value
+            ecommerce_data["is_exclusive"] = offer.exclusive
+            ecommerce_data["courses_sku"] = []
+            for product in offer.condition.range.all_products():
+                ecommerce_data["courses_sku"].append(product.stockrecords.first().partner_sku)
+
+        # if offer and len(offer.condition.range.all_products()) == 1 and offer.benefit.type == 'Percentage':
+        #     site =  Site.objects.get_current()
+        #     product = offer.condition.range.all_products()[0]
+        #     sku = product.stockrecords.first().partner_sku
+        #     data = {'discount_percentage': float(response.value)}
+        #     commerce_api_client = site.siteconfiguration.lhub_commerce_api_client
+        #     update_discount_response  = commerce_api_client.update_discount(sku).post(data=data)
 
         return response
 
