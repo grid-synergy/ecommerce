0b6d838cac0f5bd674d9f2e23a786c9c2c2f2434
Abdul Rafay <arafay.knysys@gmail.com>
2021-05-18 07:31:42 +0000

    LMA-535: forked vouchers app inside dashboard directory from virtual environment

---
+++ ecommerce/extensions/dashboard/vouchers/apps.py
@@ -0,0 +1,5 @@
+import oscar.apps.dashboard.vouchers.apps as apps
+
+
+class VouchersDashboardConfig(apps.VouchersDashboardConfig):
+    name = 'edx.app.ecommerce.ecommerce.ecommerce.extensions.dashboard.vouchers'
