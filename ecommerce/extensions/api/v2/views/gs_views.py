e37e4cb043ae6136a4e1a25bb86a1b58ed5bbcbc
Abdul Rafay <arafay.knysys@gmail.com>
2021-05-27 16:28:05 +0500

    LMA-535: Voucher Apply API for mobile

--- ecommerce/extensions/api/v2/views/gs_views.py
+++ ecommerce/extensions/api/v2/views/gs_views.py
@@ -1,3 +1,4 @@
+from ecommerce.extensions import voucher
 from ecommerce.core.models import User
 from rest_framework.response import Response
 import logging
@@ -10,7 +11,7 @@ from rest_framework.permissions import   IsAuthenticated
 import stripe
 from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthentication
 import logging
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect, HttpResponseBadRequest
 from oscar.core.loading import get_class, get_model
 from rest_framework import generics, status, viewsets
 
@@ -23,11 +24,20 @@ from edx_rest_api_client.client import EdxRestApiClient
 from oscar.apps.partner import strategy
 from django.contrib.auth import get_user_model
 from oscar.apps.basket.views import * 
-from ecommerce.extensions.basket.utils import apply_offers_on_basket
+from ecommerce.extensions.basket.utils import apply_offers_on_basket, validate_voucher, apply_voucher_on_basket_and_check_discount
 from ecommerce.extensions.api.serializers import BasketSerializer, OrderSerializer
 from ecommerce.extensions.api.throttles import ServiceUserThrottle
-
+from ecommerce.extensions.basket.exceptions import VoucherException, RedirectException
 import sys, os
+from ecommerce.extensions.offer.dynamic_conditional_offer import DynamicPercentageDiscountBenefit
+from ecommerce.extensions.api.handlers import jwt_decode_handler
+
+import urllib
+from ecommerce.extensions.offer.utils import get_redirect_to_email_confirmation_if_required
+from ecommerce.enterprise.utils  import get_enterprise_customer_from_voucher
+from django.utils.translation import ugettext as _
+from edx_rest_framework_extensions.auth.bearer.authentication import BearerAuthentication
+from rest_framework import status
 
 Applicator = get_class('offer.applicator', 'Applicator')
 Basket = get_model('basket', 'Basket')
@@ -40,9 +50,6 @@ User = get_user_model()
 Voucher = get_model('voucher', 'Voucher')
 OrderTotalCalculator = get_class('checkout.calculators', 'OrderTotalCalculator')
 NoShippingRequired = get_class('shipping.methods', 'NoShippingRequired')
-from ecommerce.extensions.offer.dynamic_conditional_offer import DynamicPercentageDiscountBenefit
-from ecommerce.extensions.api.handlers import jwt_decode_handler
-from edx_rest_framework_extensions.auth.bearer.authentication import BearerAuthentication
 
 
 
@@ -247,4 +254,197 @@ def get_course_discount_info(request, sku):
              pass
 
     return Response(discount_info)
+
+
+
+    """ Apply Voucher Mobile API Functions """
+
+def _verify_basket_not_empty(request, code):
+    username = request.user and request.user.username
+    if request.basket.is_empty:
+        logger.warning(
+            '[Code Redemption Failure] User attempted to apply a code to an empty basket. '
+            'User: %s, Basket: %s, Code: %s',
+            username, request.basket.id, code
+        )
+        raise VoucherException()
+    return True
+
+
+def _verify_voucher_not_already_applied(request, code):
+    username = request.user and request.user.username
+    if request.basket.contains_voucher(code):
+        logger.warning(
+            '[Code Redemption Failure] User tried to apply a code that is already applied. '
+            'User: %s, Basket: %s, Code: %s',
+            username, request.basket.id, code
+        )
+        messages.error(
+            request,
+            _("You have already added coupon code '{code}' to your basket.").format(code=code),
+        )
+        raise VoucherException()
+    return True
+
+
+def _get_stock_record(request, code):
+    # TODO: for multiline baskets, select the StockRecord for the product associated
+    # specifically with the code that was submitted.
+    basket_lines = request.basket.all_lines()
+    return basket_lines[0].stockrecord
+
+
+def _get_voucher(request, code):
+    try:
+        return Voucher._default_manager.get(code=code)  # pylint: disable=protected-access
+    except Voucher.DoesNotExist:
+        messages.error(request, _("Coupon code '{code}' does not exist.").format(code=code))
+        raise VoucherException()
+
+
+def _verify_email_confirmation(request, voucher, product):
+    offer = voucher.best_offer
+    redirect_response = get_redirect_to_email_confirmation_if_required(request, offer, product)
+    if redirect_response:
+        raise RedirectException(response=redirect_response)
+
+
+def _verify_enterprise_needs(request, voucher, code, stock_record):
+    if get_enterprise_customer_from_voucher(request.site, voucher) is not None:
+        # The below lines only apply if the voucher that was entered is attached
+        # to an EnterpriseCustomer. If that's the case, then rather than following
+        # the standard redemption flow, we kick the user out to the `redeem` flow.
+        # This flow will handle any additional information that needs to be gathered
+        # due to the fact that the voucher is attached to an Enterprise Customer.
+        params = urllib.parse.urlencode(
+            OrderedDict([
+                ('code', code),
+                ('sku', stock_record.partner_sku),
+                ('failure_url', request.build_absolute_uri(
+                    '{path}?{params}'.format(
+                        path=reverse('basket:summary'),
+                        params=urllib.parse.urlencode(
+                            {
+                                CONSENT_FAILED_PARAM: code
+                            }
+                        )
+                    )
+                ))
+            ])
+        )
+        redirect_response = HttpResponseRedirect(
+            request.build_absolute_uri(
+                '{path}?{params}'.format(
+                    path=reverse('coupons:redeem'),
+                    params=params
+                )
+            )
+        )
+        raise RedirectException(response=redirect_response)
+
+
+def _validate_voucher(request, voucher):
+    username = request.user and request.user.username
+    is_valid, message = validate_voucher(voucher, request.user, request.basket, request.site)
+    if not is_valid:
+        logger.warning('[Code Redemption Failure] The voucher is not valid for this basket. '
+                        'User: %s, Basket: %s, Code: %s, Message: %s',
+                        username, request.basket.id, voucher.code, message)
+        messages.error(request, message)
+        request.basket.vouchers.remove(voucher)
+        raise VoucherException()
+    return True
+
+
+def _apply_voucher(request, voucher):
+    username = request.user and request.user.username
+    valid, message = apply_voucher_on_basket_and_check_discount(voucher, request, request.basket)
+    if not valid:
+        logger.warning('[Code Redemption Failure] The voucher could not be applied to this basket. '
+                        'User: %s, Basket: %s, Code: %s, Message: %s',
+                        username, request.basket.id, voucher.code, message)
+        messages.warning(request, message)
+        request.basket.vouchers.remove(voucher)
+        return False
+    else:
+        messages.info(request, message)
+        return True
+
+
+def verify_and_apply_voucher(request, code):
+    request.basket = Basket.objects.filter(owner=request.user, status="Commited").last()
+    request.basket.strategy = request.strategy
+   
+    _verify_basket_not_empty_message = "User attempted to apply a code to an empty basket."
+    _verify_voucher_not_already_applied_message = "User tried to apply a code that is already applied."
+    _get_voucher_message = "Coupon code does not exist."
+    _validate_voucher_message = "The voucher is not valid for this basket."
+    _apply_voucher_message = "The voucher could not be applied to this basket."
+
+    voucher_applied = False
+    try:
+        if _verify_basket_not_empty(request, code):
+            _verify_basket_not_empty_message = ""
+
+        if _verify_voucher_not_already_applied(request, code):
+            _verify_voucher_not_already_applied_message = ""
+
+        stock_record = _get_stock_record(request, code)
+        voucher = _get_voucher(request, code)
+        if voucher:
+            _get_voucher_message = ""
+            benefit_type = voucher.offers.all()[0].benefit.type
+            benefit_value = voucher.offers.all()[0].benefit.value
+            if benefit_type == "Percentage":
+                basket_total_discounts_string = "-" + str(round(benefit_value, 2)) + "%"
+            else:
+                basket_total_discounts_string = "-S$" + str(round(benefit_value, 2))
+
+        _verify_email_confirmation(request, voucher, stock_record.product)
+        _verify_enterprise_needs(request, voucher, code, stock_record)
+
+        request.basket.clear_vouchers()
+        if _validate_voucher(request, voucher):
+            _validate_voucher_message = ""
+
+        if _apply_voucher(request, voucher):
+            _apply_voucher_message = ""
+            voucher_applied = True
+
+        gst_amount = request.basket.total_incl_tax - request.basket.total_excl_tax
+        gst_amount = str(round(gst_amount,2))
+        basket_total_excl_tax_excl_discounts_string = request.basket.total_excl_tax_excl_discounts
+        basket_total_excl_tax_excl_discounts_string = str(round(basket_total_excl_tax_excl_discounts_string, 2))
+        basket_total_incl_tax_string = request.basket.total_incl_tax
+        basket_total_incl_tax_string = str(round(basket_total_incl_tax_string ,2))
+
+        data = {}
+        data["discount"] = basket_total_discounts_string
+        data["subtotal"] = basket_total_excl_tax_excl_discounts_string
+        data["gst"] = gst_amount
+        data["total"] = basket_total_incl_tax_string
+
+    except VoucherException:
+        message = _verify_basket_not_empty_message or _verify_voucher_not_already_applied_message or _get_voucher_message or _get_voucher_message or _validate_voucher_message or _apply_voucher_message
+        voucher_applied = False
+
+        if not voucher_applied:
+            data = {"message": message}
+
+
+    return data
+
+
+@api_view(('POST',))
+def apply_voucher_mobile(request):
+    code = request.data.get('voucher')
+    code = code.strip()
+    data = verify_and_apply_voucher(request, code)
+
+    if "message" in data.keys():
+        return Response({'status_code' : 400, 'message': data["message"]})
+    else:
+        return Response({'status_code' : 200, 'data': data})
+
+
 
